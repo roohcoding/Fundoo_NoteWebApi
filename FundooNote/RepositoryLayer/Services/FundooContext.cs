aaa32c8cba739f9efb5dcfa99a39fecf53a5c268
Rupali Natewad <rupalirnatewad@gmail.com>
2022-06-23 16:48:57 +0530

    uploading adduser migration file

---
+++ FundooNote/RepositoryLayer/Services/FundooContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Services.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Services
+{
+
+    public class FundooContext : DbContext
+    {
+        public FundooContext(DbContextOptions<FundooContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+
+}
